45b158ff1bab198ac9ef22133de8d50e22af7493
YunJinghaibao <39584212+YunJinghaibao@users.noreply.github.com>
2019-06-07 10:14:58 +0800

    0607

---
+++ config/mysql-config.js
@@ -0,0 +1,9 @@
+module.exports = class MySQLConfig {
+    constructor(){
+        this.host = 'localhost';
+        this.user = 'root';
+        this.port = '3306';
+        this.password = '123456';
+        this.database = 'project';
+    }
+}
